6157de88630dc3f6a6b6c8aa47a02cd58133bf7b
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:22:06 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Boiling Water/Program.cs
@@ -0,0 +1,28 @@
+﻿    namespace Boiling_Water
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string ticketType = Console.ReadLine();
+            double studenPrice = 1.00;
+            double regularPrice = 1.60;
+
+
+
+
+            if (ticketType == "student")
+            {
+                Console.WriteLine($"${studenPrice:F2}");
+            }
+            else if (ticketType == "regular")
+            {
+                Console.WriteLine($"${regularPrice:F2}");
+            }
+            else
+                Console.WriteLine("Invalid ticket type!");
+           
+           
+            }
+        }
+    }
