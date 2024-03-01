89058a4d8620497dae0595a54eccb8cd0b1fd150
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:43:57 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Exam Preparation/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+int number = int.Parse(Console.ReadLine());
+int evenDigitSum = 0;
+
+
+while (number > 0)
+{
+    for (int i = 0; i < number; i++)
+    {
+        int digit = number % 10;
+        int currentNUm = number / 10;
+        number = currentNUm;
+        if (digit % 2 == 0)
+        {
+            int factorial = 1;
+            for (int j = 1; j <= digit; j++)
+            {
+                factorial = factorial * j;
+
+            }
+            evenDigitSum = factorial + evenDigitSum;
+        }
+    }
+}
+Console.WriteLine(evenDigitSum);
