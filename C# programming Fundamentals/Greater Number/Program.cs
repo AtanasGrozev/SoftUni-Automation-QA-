7070cabb428efc3aa49ee2de2e6edf261c1cc433
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:22:41 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Greater Number/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string extra = Console.ReadLine();
+
+            double price = 0;
+            if (drink == "coffee")
+                price = 1.00;
+            else if (drink == "tea")
+            {
+                price = 0.60;
+
+            }
+            if (extra == "sugar")
+            {
+                price = price + 0.40;
+            }
+            else if (drink == "no")
+            {
+                price = price + 0.00;
+            }
+            Console.WriteLine($"Final price: ${price:f2}");
+        }
+
+    }
+
+}
