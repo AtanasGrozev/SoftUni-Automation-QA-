b7a4ba4371cb499dd46e717d63174f5fbc7a78db
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:24:43 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Speed Info/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Transactions;
+
+namespace Speed_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = a * b;
+                Console.WriteLine($"{area:f2}");
+            }
+            else if (type == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = a * b;
+                Console.WriteLine($"{area:f2}"); ;
+
+            }
+            else if(type == "circle")
+            {
+                double rad = double.Parse(Console.ReadLine());
+                double area = rad * rad * Math.PI;
+                Console.WriteLine($"{area:F2}");
+            }
+            else 
+                    {
+                Console.WriteLine("Invalid figure.");
+            }
+
+        }
+    }
+}
