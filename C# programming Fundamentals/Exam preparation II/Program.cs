eb785d3dfed5c24188c0605a6ebfd209043abe3d
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:44:50 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Exam preparation II/Program.cs
@@ -0,0 +1,45 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+
+bool noPrimeNumber = true;
+
+
+
+
+for (int i = 1; i <= number; i++)
+{
+    int currentNumber = i;
+    int digitSum = 0;
+    bool IsAllDigitsPrime = true;
+
+
+    while (currentNumber > 0)
+    {
+        int digit = currentNumber % 10;
+        currentNumber = currentNumber / 10;
+        bool isDigitPrime = digit == 2 || digit == 3 || digit == 5 || digit == 7;
+        if (isDigitPrime)
+        {
+            digitSum = digitSum + digit;
+        }
+
+        else
+        {
+            IsAllDigitsPrime = false;
+            break;
+        }
+    }
+    if (IsAllDigitsPrime && digitSum % 2 == 0)
+    {
+        Console.Write(i + " ");
+        noPrimeNumber = false;
+    }
+}
+if (noPrimeNumber)
+{
+    Console.WriteLine("no");
+}
+
+
+
+
