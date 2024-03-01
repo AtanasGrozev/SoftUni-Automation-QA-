f1861946f6e07b045952f6de98a30f34c44ef2e0
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:33:34 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Student Academy/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, List<double>> students = new ();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string student = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());   
+    
+    if (!students.ContainsKey(student))
+    {      
+        students.Add(student, new List<double>());
+       
+    } 
+        students[student].Add(grade);
+
+}
+var result = students.Where(x => x.Value.Average() >= 4.50);
+foreach (var currentstudent in result)
+{
+    Console.WriteLine($"{currentstudent.Key} -> {currentstudent.Value.Average():F2}");
+
+}
