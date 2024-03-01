638b78b1bf1d22b8a5c932b92ddd1fbc72d30b4c
AtanasGrozev <136845351+AtanasGrozev@users.noreply.github.com>
2024-03-01 16:20:03 +0200

    Add files via upload

---
+++ C# programming Fundamentals/Condense Array to Number/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+List<int> input = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+
+while (input.Count > 1)
+{
+    List<int> output = new List<int>();
+
+    for (int i = 0; i < input.Count - 1; i++)
+    {
+        int num1 = input[i];
+        int num2 = input[i + 1];
+        int sum = num1 + num2;
+        output.Add(sum);
+      
+      
+    }
+    input = output;
+ 
+
+}
+Console.WriteLine(input[0]);
+
+
+
+
+
